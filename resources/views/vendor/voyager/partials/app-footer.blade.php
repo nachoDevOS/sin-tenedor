4b5efed6486c4d57c6698abcdf48000369ccfbd1
nachoDevOs <ignaciomolinaguzman20@gmail.com>
2025-05-29 11:04:16 -0400

    Eiminacion de Ventas

--- resources/views/vendor/voyager/partials/app-footer.blade.php
+++ resources/views/vendor/voyager/partials/app-footer.blade.php
@@ -1,9 +1,10 @@
 <footer class="app-footer">
     <div class="site-footer-right">
-        {!! __('voyager::theme.footer_copyright') !!} <a href="http://thecontrolgroup.com" target="_blank">The Control Group</a>
+        {{-- {!! __('voyager::theme.footer_copyright') !!} <a href="http://thecontrolgroup.com" target="_blank">The Control Group</a>
         @php $version = Voyager::getVersion(); @endphp
         @if (!empty($version))
             - {{ $version }}
-        @endif
+        @endif --}}
+        GastroVentas v1.0
     </div>
 </footer>
