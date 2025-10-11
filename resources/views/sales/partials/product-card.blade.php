43d8bc04f3efee4449efe1a2d3a000a9ebefa075
nachoDevOs <ignaciomolinaguzman20@gmail.com>
2025-10-11 12:10:46 -0400

    dev

--- resources/views/sales/partials/product-card.blade.php
+++ resources/views/sales/partials/product-card.blade.php
@@ -13,7 +13,7 @@
             <img src="{{ $image }}" class="img-responsive">
             <div class="product-info">
                 {{-- <h5>{{ $product->name }}</h5> --}}
-                <p>{{Str::words($product->name, 20, '...')}}</p>
+                <p>{{Str::limit($product->name, 20, '...')}}</p>
                 <p class="text-success">Bs. {{ number_format($product->price, 2, ',', '.') }}</p>
                 @if ($product->typeSale == 'Venta Con Stock')
                     Stock: <small class="{{ $cantStock == 0 ? 'text-danger' : '' }}">{{ number_format($cantStock, 2, ',', '.') }}</small>
